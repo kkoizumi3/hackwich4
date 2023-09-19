c4c497314a2f18b84edae392339e027eb0ab3d46
Kaci Koizumi <koizumi3@hawaii.edu>
2023-09-19 13:51:25 -1000

    completed parts 1-9 need to do on your own

--- hackwich4/ViewController.swift
+++ hackwich4/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
+        self.firstLabel.text = "about"
     }
-
+    
 
 }
 
